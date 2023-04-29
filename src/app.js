3bf31e1225b60903b07326ab305edc9fb77b3fa0
encaladach <119975587+encaladach@users.noreply.github.com>
2023-04-29 00:11:18 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+
+  console.log("Hello Rigo from the console!" + randomNumber);
 };
